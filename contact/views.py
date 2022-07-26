063736b3041996c9f1ba988e267c59d5869eb11d
trevin <60538411+trevin-livele@users.noreply.github.com>
2022-07-26 12:26:22 +0300

    added contact

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.forms import ContactForm
 
 # Create your views here.
+
+
+def contact(request):
+    form = ContactForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('contact')
+    else:
+        form = ContactForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'home.html', context)

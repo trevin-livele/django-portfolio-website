b27e2594830140b938d75f514e00535d7c7bce55
trevin <60538411+trevin-livele@users.noreply.github.com>
2022-07-26 11:45:31 +0300

    added contact model

--- contact/models.py
+++ contact/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    subject = models.CharField(max_length=50)
+    message = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name

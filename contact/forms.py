db852b59974581eb0cb5b94fc7ab960f34e95feb
trevin <60538411+trevin-livele@users.noreply.github.com>
2022-08-01 14:19:51 +0300

    added ixes

--- contact/forms.py
+++ contact/forms.py
@@ -5,4 +5,10 @@ from .models import Contact
 class ContactForm(ModelForm):
     class Meta:
         model = Contact
-        fields = '__all__'
+        fields = ['name','email','subject','message']
+
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args,**kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
--- contact/models.py
+++ contact/models.py
@@ -6,7 +6,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     subject = models.CharField(max_length=50)
-    message = models.CharField(max_length=250)
+    message = models.TextField(max_length=250)
    
     def __str__(self):
         return self.name

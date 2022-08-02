8cf83199658abbeebc11343e6bcad9cbb1ebb2d8
trevin <60538411+trevin-livele@users.noreply.github.com>
2022-08-02 11:37:34 +0300

    added cloudinary

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,6 +45,8 @@ INSTALLED_APPS = [
     'categories',
     'appmain',
     'contact',
+    'cloudinary'
+
 ]
 
 MIDDLEWARE = [
